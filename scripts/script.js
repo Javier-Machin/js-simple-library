f5e6bba3c39b0d67e0920ff265fddff4b8daa405
Javier Machin <javier_machin_acosta@hotmail.com>
2018-08-01 13:14:08 +0100

    Add books form show/hide toggle button, add delete button to each book

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,52 @@
 const quixote = new Book("Don Quixote", "Cervantes", 980, false);
 const artOfWar = new Book("The Art of War", "Sun Tzu", 130, false);
 const books = [quixote, artOfWar];
+const submitBtn = document.getElementById("form-submit");
+const newBookBtn = document.getElementById("new-book-button");
+const bookForm = document.querySelector(".book-form");
+
+submitBtn.addEventListener("click", function() {
+  addBook(books);
+  render(books);
+})
+
+newBookBtn.addEventListener("click", function() {
+  bookForm.classList.toggle("visible");
+})
+
 
 function render(books) {
-  mainContainer = document.querySelector(".main-container") 
+  const mainContainer = document.querySelector(".main-container");
+  let booksContainer = document.querySelector(".books-container");
+  
+  // Removes books container to prevent listing books more than once
+  if (booksContainer) { mainContainer.removeChild(booksContainer); }
+
+  booksContainer = document.createElement("section");
+  booksContainer.className = "books-container";
+  mainContainer.appendChild(booksContainer);
   
   books.forEach(function(book) {
     const bookInfo = `${book.title}, ${book.author}, 
                       ${book.pages}, ${book.read}`;
     const node = document.createElement("article");
     const textNode = document.createTextNode(bookInfo); 
+    
+    // Add a delete button for each book
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete Book";
+    deleteBtn.addEventListener("click", function() {
+      booksContainer.removeChild(node);
+      const bookToDelete = books.indexOf(book);
+      if (bookToDelete > -1) {
+        books.splice(bookToDelete, 1);
+      }
+    })
+
     node.className = "book";                 
-    node.appendChild(textNode);                              
-    mainContainer.appendChild(node);
+    node.appendChild(textNode);
+    node.appendChild(deleteBtn);                              
+    booksContainer.appendChild(node);
   })
 }
 
@@ -33,3 +67,4 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+render(books);
